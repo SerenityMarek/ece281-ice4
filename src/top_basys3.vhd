091f65f13090919e9d4d23c8cc872fab3d35a199
SerenityMarek <c28serenity.marek@afacademy.af.edu>
2026-04-15 11:05:03 -0600

    new waveform

--- src/top_basys3.vhd
+++ src/top_basys3.vhd
@@ -13,7 +13,7 @@
 --| FILENAME      : top_basys3.vhd
 --| AUTHOR(S)     : Capt Phillip Warner
 --| CREATED       : 02/22/2018 Modified: 03/01/2020 by capt Dan Johnson
---| DESCRIPTION   : This file implements the top level module for the solution for Stoplight FSM.
+--| DESCRIPTION   : This file implements the top level module for the solution for  FSM.
 --|
 --|					Inputs:  clk 	--> 100 MHz clock from FPGA
 --|                          sw(0)  --> car present
